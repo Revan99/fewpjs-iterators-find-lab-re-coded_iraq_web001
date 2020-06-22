10984b6762712dd1d5ba1fe834db00dc86dc8c33
Revan Sarbast <revansarbast8@gmail.com>
2020-06-22 08:52:27 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
-const testVar = {}
+const testVar = {
+  year:12,
+  result:'w'
+}
 
 function testFunc() {
   return "hi"
+}
+function superbowlWin(arr){
+  for(let element of arr){
+    if(element.result =="W"){
+      return element.year
+    }
+    alert('nothing like this found')
+  }
 }
