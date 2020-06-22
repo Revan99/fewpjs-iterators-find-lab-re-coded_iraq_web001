10affab9cd46e799eeadf456be199b5ec9f91b24
Revan Sarbast <revansarbast8@gmail.com>
2020-06-22 08:50:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const testVar = {
   year:12,
-  reult:12
+  result:'w'
 }
 
 function testFunc() {
   return "hi"
 }
 function superbowlWin(arr){
-  
+  for(element of arr){
+    if(element.result ==="w"){
+      return arr.year
+    }
+    alert('nothing like this found')
+  }
 }
